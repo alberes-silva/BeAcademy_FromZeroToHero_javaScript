ae23d2e802ddee02929818dd65719c033d51cf45
Alberes Silva <alberessilva@yahoo.com.br>
2022-11-14 12:01:02 -0300

    Aula 3 explicações sobre tipos de dados (String, number, booler, undefined, null, objetos, array)

---
+++ script.js
@@ -0,0 +1,36 @@
+//Comentário em linha
+/*
+    Comentário em bloco
+*/
+
+console.log('Olha eu no "aspas" simples!!')  //String
+
+console.log(12)  //Integer
+console.log(24.5) // Float
+console.log(12*24/4) // calc
+//console.log(infinity)
+console.log(50+(53*4.5))
+console.log(Math.round(50+(53*4.5)))
+
+console.log(24 > 32)
+console.log(42 > 32)
+
+//objetos
+console.log({
+    marca: 'FIAT',
+    largura: '550cm',
+    peso:1,
+    velocidade: (90*200)*2,
+    andar: function(){
+        console.log(`Sua velocidade é de 200`)
+    },
+    possuiAr: true
+})
+
+//Array
+console.log([
+    "Bolacha",
+    "Coca-cola",
+    "Verduras",
+    "água"
+])
