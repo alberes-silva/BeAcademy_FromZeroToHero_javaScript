1191245f898a678d8790d745303b9e995c93b91f
Alberes Silva <alberessilva@yahoo.com.br>
2022-11-15 14:05:02 -0300

    Função I, explicações sobre funções com parâmetros e argumentos, e função com retorno

--- funcao.js
+++ funcao.js
@@ -9,12 +9,17 @@ function myFristFunction() {
 //Execute
 myFristFunction()
 
+//Função com parâmetros e argumentos
+
 //number1 = 32 e number2 = 24
 function sum(number1, number2){
     console.log(number1 + number2)
 }
 
 sum(32, 24)
+
+
+//Função com retorno
 
 function myFristFunctionWithReturn(num1, num2){
     let total = num1 + num2
