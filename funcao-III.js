8235d83e9858294d05bd95aa5c425ac4e73f78df
Alberes Silva <alberessilva@yahoo.com.br>
2022-11-17 10:06:51 -0300

    Função III, com explicação sobre funções anônimas e setTimeout

---
+++ funcao-III.js
@@ -0,0 +1,9 @@
+//Função Anônima
+
+(function () {
+    console.log('Paylivre S2')
+})()//Autoexecução
+
+setTimeout (function(){
+    console.log('Vou levar 3 segundos para ser executado')
+},3000)
