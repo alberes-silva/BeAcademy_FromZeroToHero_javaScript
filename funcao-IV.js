34d8c658cc5d8bb4489ae4c68399fff66da1cccc
Alberes Silva <alberessilva@yahoo.com.br>
2022-11-17 10:46:17 -0300

    Função IV, explicação sobre escopos gerais e locais

---
+++ funcao-IV.js
@@ -0,0 +1,32 @@
+//Parte 1
+
+// let assunto
+
+// function criarAssunto(assunto){
+//     return assunto
+// }
+
+// console.log(criarAssunto(assunto));
+
+//Parte 2
+
+// let assunto = 'Estudar'  //var = valor
+
+// function criarAssunto(assunto){
+//     return assunto
+// }
+
+// console.log(assunto) //Resposta => estudar
+// console.log(criarAssunto(assunto)); //Resposta => estudar
+
+//Parte 3
+
+let assunto = 'Estudar'  //var = valor
+
+function criarAssunto(assunto){
+    assunto = "Escutar música" //Mudança de valor
+    return assunto
+}
+
+console.log(assunto) //Resposta => 'Estudar'
+console.log(criarAssunto(assunto)); //Resposta => 'Escutar Música'
