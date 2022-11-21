258a117470845eec23b0f437d5d0d3a87769dc05
Alberes Silva <alberessilva@yahoo.com.br>
2022-11-21 09:48:25 -0300

    Manipulando Array

--- dados-array.js
+++ dados-array.js
@@ -44,5 +44,42 @@
 
 //Transformando um array em caracteres
 
-let wordToArray = "Quem ta gostando diz Hey!!"
-console.log(Array.from(wordToArray))
+// let wordToArray = "Quem ta gostando diz Hey!!"
+// console.log(Array.from(wordToArray))
+
+//Manipylando Arrays
+const animals = ["pinto","Urso","Elefante","Girafa","Leão"]
+console.log(animals)
+
+//Adicionar item no fim
+
+console.log(animals.push("Rato"))
+console.log(animals)
+
+//Adicionar item no inicio
+console.log(animals.unshift("Porco"))
+console.log(animals)
+
+//Remover item no fim
+console.log(animals.pop())
+console.log(animals)
+
+//Remover item do inicio
+console.log(animals.shift())
+console.log(animals)
+
+//Pegar somente alguns elementos
+//Não modificar o array
+
+console.log(animals.slice(0,3))
+console.log(animals)
+
+//Remover um item ou mais do Array
+// animals.splice(1,3)
+console.log(animals)
+
+//Encontrar a posição do array
+let index = animals.indexOf('Leão')
+console.log(index)
+animals.splice(index,1)
+console.log(animals)
